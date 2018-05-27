a490ca521467e54407beee5e7c735e1e50f832fd
SiarheiGribov <bars232@gmail.com>
2018-05-27 17:14:24 +0300

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -27,7 +27,6 @@ for line in signData:
     line = str(line.decode('UTF-8').rstrip('\n'))
     if re.match(r'^\*', line):
         signList.append('{{Hello}} ' + re.sub(r'^\*\s', '', line) + ' ~~~~~')
-
 # Получаем список юзеров из свежих правок
 payload = {'action': 'query', 'format': 'json', 'list': 'recentchanges',
 'rcprop': 'user|timestamp', 'rcshow': '!bot|!anon', 'rctype': 'new|edit', 'rcend': (datetime.datetime.now() - datetime.timedelta(minutes = minutes)).strftime("%Y-%m-%d %H:%M:%S"), 'rclimit': 5000, 'token': token}
@@ -40,7 +39,6 @@ for user in users:
     if user['user'] not in usersCheck:
         usersCheck.append(user['user'])
         usersList += user['user'] + '|'
-
 # Проверяем число правок и не заблокирован ли юзер
 payload = {'action': 'query', 'format': 'json', 'utf8': '', 'list': 'users', 'ususers': usersList.rstrip('|'), 'usprop': 'blockinfo|editcount', 'token': token}
 r_userinfo = json.loads(requests.post('https://ru.wikipedia.org/w/api.php', data=payload, cookies=cookies).text)
@@ -55,8 +53,10 @@ for user in userinfo:
             isdelete = ast.literal_eval('{query}'.format(**r_isdelete))
             isdelete = ast.literal_eval('{logevents}'.format(**isdelete))
             if len(isdelete) == 0:
-                # Публикуем приветствие, если страница ещё не создана
-                random_index = randrange(0, len(signList))
-                sign = signList[random_index]
-                payload = {'action': 'edit', 'format': 'json', 'title': 'User talk:' + user['name'], 'utf8': '', 'createonly': '', 'notminor': '', 'text': sign, 'summary': u'Добро пожаловать!', 'token': token}
-                r_edit = requests.post('https://ru.wikipedia.org/w/api.php', data=payload, cookies=cookies)
+                # Если имя не заканчивается на бот / bot
+                if not (re.search(r'[бот|bot]$', user['name'])):
+                    # Публикуем приветствие, если страница ещё не создана
+                    random_index = randrange(0, len(signList))
+                    sign = signList[random_index]
+                    payload = {'action': 'edit', 'format': 'json', 'title': 'User talk:' + user['name'], 'utf8': '', 'createonly': '', 'notminor': '', 'text': sign, 'summary': u'Добро пожаловать!', 'token': token}
+                    r_edit = requests.post('https://ru.wikipedia.org/w/api.php', data=payload, cookies=cookies)
