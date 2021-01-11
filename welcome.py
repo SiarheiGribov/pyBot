16313da91bcbaaccff52502c10b5c19152784a63
SiarheiGribov <bars232@gmail.com>
2021-01-12 01:42:11 +0300

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+﻿# coding=utf-8
 
 import sys
 reload(sys)
@@ -7,26 +7,42 @@ sys.path.append('pyBot/ext_libs')
 import re
 import ast
 import json
-import time
 import login
-import datetime
 import requests
 from urllib2 import urlopen
 from random import randrange
-from sseclient import SSEClient as EventSource
 
-minutes = 23
-signUrl = 'https://ru.wikipedia.org/w/?action=raw&utf8=1&title=User:LatitudeBot/Sign'
+signUrl = 'https://ru.wikipedia.org/?curid=1006803&action=raw&section=1'
+signUrlMentors = 'https://ru.wikipedia.org/?curid=8409309&action=raw&section=1'
+
+# Список подписей и чёрный список
+signList = []
+preSignList = {}
+defaultPhrase = "При вопросах можете обратиться к $ "
+
+signData = urlopen(signUrl).readlines()
+signDataMentors = urlopen(signUrlMentors).readlines()
 
 token, cookies = login.login()
 
 # Список подписей и чёрный список
-signList = []
-signData = urlopen(signUrl).readlines()
+
 for line in signData:
     line = str(line.decode('UTF-8').rstrip('\n'))
-    if re.match(r'^\*', line):
-        signList.append('{{Hello}} ' + re.sub(r'^\*\s', '', line) + ' ~~~~~')
+    if re.match(r'^\*\s*?\[\[Участни(?:к|ца):(.*?)\]\]\s*?\|(.*)', line):
+        r = re.match(r'^\*\s*?\[\[(Участни(?:к|ца)):(.*?)\]\]\s*?\|(.*)', line)
+        preSignList[r.group(2)] = {"gender": r.group(1), "phrase": r.group(3).lstrip().rstrip() + " — ~~~~~"}
+for line in signDataMentors:
+    line = str(line.decode('UTF-8').rstrip('\n'))
+    if re.match(r'^\*\s*?\[\[Участни(?:к|ца):(.*?)\]\]\s*?\|(.*)', line):
+        r = re.match(r'^\*\s*?\[\[(Участни(?:к|ца)):(.*?)\]\]\s*?\|(.*)', line)
+        if r.group(2) not in preSignList:
+            preDefaultPhrase = defaultPhrase.replace("$", "участнику") if (r.group(1) == "Участник") else defaultPhrase.replace("$", "участнице")
+            preSignList[r.group(2)] = {"gender": r.group(1), "phrase": preDefaultPhrase + "[[ОУ:" + r.group(2) + "|"
+                                       + r.group(2) + "]] — ~~~~~"}
+for k in preSignList:
+    signList.append('{{Hello}} ' + preSignList[k]["phrase"])
+
 badList = []
 URL_BL = 'https://ru.wikipedia.org/w/?action=raw&utf8=1&title=User:IluvatarBot/Badlist'
 bl = urlopen(URL_BL).readlines()
@@ -34,16 +50,30 @@ for line in bl:
     line = str(line.decode('UTF-8').rstrip('\n'))
     if re.match(r'^\*', line):
         badList.append(re.sub(r'^\*', '', line))
+
+with open('pyBot/timeWelcome.txt') as Lines:
+    line = Lines.read().splitlines()
+timestamp = line[0]
+rcid = line[1]
+
 # Получаем список юзеров из свежих правок
+# timestamp = ((datetime.datetime.utcnow() - datetime.timedelta(minutes = minutes)).strftime("%Y-%m-%d %H:%M:%S")
 payload = {'action': 'query', 'format': 'json', 'list': 'recentchanges',
-'rcprop': 'user|timestamp', 'rcshow': '!bot|!anon', 'rctype': 'new|edit', 'rcend': (datetime.datetime.now() - datetime.timedelta(minutes = minutes)).strftime("%Y-%m-%d %H:%M:%S"), 'rclimit': 5000, 'token': token}
+'rcprop': 'user|timestamp|ids', 'rcshow': '!bot|!anon', 'rctype': 'new|edit', 'rcend': timestamp, 'rclimit': 5000}
 r_changes = json.loads(requests.post('https://ru.wikipedia.org/w/api.php', data=payload, cookies=cookies).text)
-users = ast.literal_eval('{query}'.format(**r_changes))
-users = ast.literal_eval('{recentchanges}'.format(**users))
+data = ast.literal_eval('{query}'.format(**r_changes))
+users = ast.literal_eval('{recentchanges}'.format(**data))
+
 usersCheck = []
 usersList = ''
+timestampNow = users[0]["timestamp"]
+rcidNow = users[0]["rcid"]
+time_file = open("pyBot/timeWelcome.txt", "w")
+time_file.write(str(timestampNow) + "\n" + str(rcidNow))
+time_file.close()
+
 for user in users:
-    if user['user'] not in usersCheck:
+    if user['user'] not in usersCheck and user["rcid"] != rcid:
         usersCheck.append(user['user'])
         usersList += user['user'] + '|'
 # Проверяем число правок и не заблокирован ли юзер
@@ -61,7 +91,7 @@ for user in userinfo:
             isdelete = ast.literal_eval('{logevents}'.format(**isdelete))
             if len(isdelete) == 0:
                 # Если имя не заканчивается на бот / bot
-                if not (re.search(r'[бот|bot]$', user['name'])):
+                if not (re.search(r'(бот|bot|Бот)$', user['name'], flags=re.I)):
                     # Публикуем приветствие, если страница ещё не создана. Также проверяем имя на наличие в нём
                     # элементов из чёрного списка и при необходимости публикуем отчёт
                     reason = ''
@@ -75,9 +105,9 @@ for user in userinfo:
                         raport = urlopen(URL_RAPORT).readlines()
                         for line in raport:
                             if not line.decode("utf-8").strip('\r\n') == '{{/header}}':
-                                new_pub.append(line.decode("utf-8").strip('\r\n'))
-                        raport_page = ''.join(map(str,new_pub))
-                        raport_page = '{{/header}}' + pre_pub + raport_page
+                                new_pub.append(line.decode("utf-8").strip('\r\n') + "\n")
+                        raport_page = ''.join(map(str, new_pub))
+                        raport_page = '{{/header}}\n' + pre_pub + "\n" + raport_page
                         payload = {'action': 'edit', 'format': 'json', 'title': 'User:IluvatarBot/Report', 'utf8': '', 'text': raport_page, 'summary': 'Выгрузка отчёта: подозрительный ник', 'token': token}
                         r_bad = requests.post('https://ru.wikipedia.org/w/api.php', data=payload, cookies=cookies)
                     random_index = randrange(0, len(signList))
