df41fb373f09b8fa40a06d95c48f052d3ef6a44b
SiarheiGribov <bars232@gmail.com>
2021-03-16 11:34:48 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,36 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import re
 import time
 import requests
 
-# Copyright © 2017, [[user:Iluvatar]], MITL; Based on https://www.mediawiki.org/wiki/User:Sebelino7/Login_with_Python
+headers = {'User-Agent': 'pyBot (iluvatarbot@tools.wmflabs.org); python (requests); Login'}
 
-def checkLogin():
-    # функця повтора логина в случае неудачи
-    print('Повторная попытка входа будет произведена через 6 минут...')
-    time.sleep(360)
+
+def login_retry():
+    time.sleep(360)  # 6 мин
     login()
 
+
 def login(server="ru.wikipedia"):
-    # Username, BotPassword, User-Agent
-    with open('pyBot/password.password') as Lines:
-        line = Lines.read().splitlines()
-    headers = {'User-Agent': 'pyBot (iluvatarbot@tools.wmflabs.org) requests / Login'}
-    # Первый этап
+    with open('pyBot/password.password') as lines:
+        lines = lines.read().splitlines()
+        username = lines[0]  # BotUsername
+        password = lines[1]  # BotPassword
+
     req = {'action': 'query', 'format': 'json', 'utf8': '', 'meta': 'tokens', 'type': 'login'}
     r1 = requests.post('https://' + str(server) + '.org/w/' + 'api.php', data=req, headers=headers)
     login_token = r1.json()['query']['tokens']['logintoken']
-    # Второй этап
-    req = {'action': 'login', 'format': 'json', 'utf8': '', 'lgname': line[0], 'lgpassword': line[1], 'lgtoken': login_token}
+
+    req = {'action': 'login', 'format': 'json', 'utf8': '', 'lgname': username, 'lgpassword': password,
+           'lgtoken': login_token}
     r2 = requests.post('https://' + str(server) + '.org/w/' + 'api.php', data=req, cookies=r1.cookies, headers=headers)
     cookies = r2.cookies
-    # Получение токена для редактирования
-    params3 = '?format=json&action=query&meta=tokens&continue='
+
+    params3 = '?format=json&action=query&meta=tokens'
     r3 = requests.get('https://' + str(server) + '.org/w/' + 'api.php' + params3, cookies=r2.cookies, headers=headers)
-    # если логин не удался (не получили токен), повторяем попытку
-    if not re.findall(r'\w{40}\+\\', r3.json()['query']['tokens']['csrftoken']):
-        print('Не удалось войти в систему. Повторите попытку.')
-        checkLogin()
+
+    if r3.json()['query']['tokens']['csrftoken'] == "+\\":
+        login_retry()
+
     token = r3.json()['query']['tokens']['csrftoken']
     return token, cookies
