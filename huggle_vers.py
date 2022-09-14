fa2485536f1c39aca6765a677a3fe7abf315a606
SiarheiGribov <iluvatar@toolforge.org>
2022-09-14 19:40:35 +0300

    Update huggle_vers.py

--- huggle_vers.py
+++ huggle_vers.py
@@ -3,22 +3,26 @@
 
 import login
 import requests
-from urllib.request import urlopen
 
-try:
-    URL_VER_ORIG = 'https://en.wikipedia.org/w/?action=raw&utf8=1&title=Template:Huggle/Version'
-    VER_ORIG = urlopen(URL_VER_ORIG).readline().decode('UTF-8').rstrip('\n')
-    URL_VER_OLD = 'https://ru.wikipedia.org/w/?action=raw&utf8=1&title=Template:Huggle/Version'
-    VER_OLD = urlopen(URL_VER_OLD).readline().decode('UTF-8').rstrip('\n')
-except:
-    print(u"Ошибка при получении содержимого шаблонов.")
-    exit()
 
-if not VER_OLD == VER_ORIG:
+def send(ver):
     token, cookies = login.login()
-    payload = {'action': 'edit', 'format': 'json', 'title': 'Template:Huggle/Version', 'utf8': '', 'text': VER_ORIG, 'summary': 'Обновление номера версии', 'token': token}
+    payload = {'action': 'edit', 'format': 'json', 'title': 'Template:Huggle/Version', 'utf8': '', 'text': ver,
+               'summary': 'Обновление номера версии', 'token': token}
     try:
-        req = requests.post('https://ru.wikipedia.org/w/api.php', data=payload, cookies=cookies)
+        requests.post('https://ru.wikipedia.org/w/api.php', data=payload, cookies=cookies)
     except:
         print(u"Ошибка при публикации")
         exit()
+
+
+try:
+    URL_VER_ORIG = 'https://en.wikipedia.org/w/?action=raw&utf8=1&title=Template:Huggle/Version'
+    VER_ORIG = requests.get(URL_VER_ORIG).text.rstrip('\n')
+    URL_VER_OLD = 'https://ru.wikipedia.org/w/?action=raw&utf8=1&title=Template:Huggle/Version'
+    VER_OLD = requests.get(URL_VER_OLD).text.rstrip('\n')
+    if VER_OLD != VER_ORIG:
+        send(VER_ORIG)
+except:
+    print(u"Ошибка при получении содержимого шаблонов.")
+    exit()
