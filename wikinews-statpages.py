a9f35aebb95751073770360462c081c423e6b526
SiarheiGribov <iluvatar@toolforge.org>
2022-09-14 19:42:04 +0300

    Update wikinews-statpages.py

--- wikinews-statpages.py
+++ wikinews-statpages.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+
 import time
 import requests
 import login
@@ -5,9 +8,9 @@ import login
 API_url = "https://ru.wikinews.org/w/api.php"
 ua = {"User-agent": "pyBot/wikinews-statpages (toolforge/iluvatarbot; iluvatar@tools.wmflabs.org) requests"}
 
-with open("statpages.txt", "r", encoding="utf-8") as f:
+with open("pyBot/wikinews/service/statpages.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
-f.close()
+    f.close()
 timestamp = str(lines[0])
 pagename = str(lines[1])
 
@@ -36,18 +39,20 @@ def handler(members, timestamp, pagename):
         timestamp = members[0]["timestamp"]
     token, cookies = login.login(server="ru.wikinews")
     for member in members:
-        if str(member["title"]) != pagename:
+        if str(member["title"]) != pagename and not str(member["title"]).startswith("Лента новостей "):
             params = {
                 "action": "edit", "format": "json", "utf8": "1",
                 "title": "Викиновости:Статистика страниц/" + str(member["title"]),
                 "createonly": 1, "text": "{{Статистика страницы|" + str(member["title"] + "}}"),
-                "summary": "Создание страницы статистики", "token": token
+                "summary": "Создание страницы статистики", "bot": 1, "token": token
             }
             requests.post(url=API_url, data=params, cookies=cookies)
+            time.sleep(5)
+            
     if pagename_n != "":
-        with open("statpages.txt", "w", encoding="utf-8") as f:
+        with open("pyBot/wikinews/service/statpages.txt", "w", encoding="utf-8") as f:
             f.write(timestamp + "\n" + pagename_n)
-        f.close()
+            f.close()
 
 
 getData()
